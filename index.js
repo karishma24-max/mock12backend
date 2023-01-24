f831e01afcf288a4969a71ac29b4e276298a77dc
karishma24-max <chauhankarishma824@gmail.com>
2023-01-24 11:57:36 +0530

    added

--- index.js
+++ index.js
@@ -6,39 +6,15 @@ const cors = require('cors');
 server.use(express.json());
 server.use(cors());
 const userRouter = require("./features/user/user.router.js")
+const jobRouter=require("./features/Jobs/job.router")
 
 server.use("/user",userRouter);
-//const User = require("./features/user/user.model");
+server.use("/jobs",jobRouter)
 
 server.get("/",(req,res)=>{
     res.send("Hello world!")
 })
 
-server.post("/calculate",async(req,res)=>{
-    let {amount,interest,years}=req.body
-    
-     try{
-       let i=((+[interest])/100)
-    
-       let cal=(((((1+i)**(+[years]))-1)/i)*(+[amount]))
-       let inves_amnt= (+[amount]) * (+[years])
-       let gain=cal-inves_amnt
-
-       res.send({maturity:(cal+""),
-       investamount:(inves_amnt+""),
-       gain_interest:(gain+"")
-    })
-              
-     }
-     catch(e)
-     {
-        res.send(e.message)
-     }    
-                                                            
-           
-           
-           
-       })
 server.listen(8080,async(req,res)=>{
    try{
     await connect();
